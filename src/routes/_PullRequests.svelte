f83784f796fd5781499b14e36d613b2eab5cd863
Jeff Balboni <jeff@adhocteam.us>
2021-09-17 13:40:12 -0400

    Styling, limit fetches

--- src/routes/_PullRequests.svelte
+++ src/routes/_PullRequests.svelte
@@ -3,21 +3,28 @@
 
   let pullRequests;
   let loading = true;
+  let lastFetch = Date.now();
   onMount(async () => {
     const response = await fetch('pullRequests.json');
     pullRequests = await response.json();
     loading = false;
 
     window.addEventListener('focus', async () => {
-      loading = true;
-      const response = await fetch('pullRequests.json');
-      pullRequests = await response.json();
-      loading = false;
+      if (Date.now() - lastFetch > 15000) {
+        loading = true;
+        const response = await fetch('pullRequests.json');
+        pullRequests = await response.json();
+        lastFetch = Date.now();
+        loading = false;
+      }
     });
   });
 </script>
 
 <h2>Pull requests</h2>
+{#if loading}
+  <span>refreshing...</span>
+{/if}
 {#if pullRequests}
   <ul>
     {#each pullRequests as pr}
@@ -29,9 +36,13 @@
     {/each}
   </ul>
 {/if}
-{#if loading}
-  <p>Loading...</p>
-{/if}
 
 <style>
+  h2 {
+    display: inline-block;
+    margin-top: 0;
+  }
+  ul {
+    margin: 0;
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,19 @@
-<script context="module">
-  export async function load({ page, fetch }) {
-    try {
-      const response = await fetch(`changeMetrics.json?path=vaos&label=vaos`);
-      const metrics = await response.json();
-
-      return {
-        props: {
-          metrics
-        }
-      };
-    } catch (e) {
-      return {
-        status: 500,
-        body: e.message
-      };
-    }
-  }
-</script>
-
 <script>
   import DeployedChanges from './_DeployedChanges.svelte';
   import LeadTime from './_LeadTime.svelte';
   import FailureRate from './_FailureRate.svelte';
   import PullRequests from './_PullRequests.svelte';
+  import { onMount } from 'svelte';
 
-  export let metrics;
+  let metrics = null;
+
+  onMount(async () => {
+    const response = await fetch(`changeMetrics.json?path=vaos&label=vaos`);
+    metrics = await response.json();
+  });
 </script>
 
 <div class="row">
-  <div class="metric-chart">
-    <DeployedChanges data={metrics.changes} />
-  </div>
-  <div class="metric-chart">
-    <LeadTime data={metrics.leadTime} />
-  </div>
-  <div class="metric-chart">
-    <FailureRate data={metrics} />
-  </div>
   <div class="metric-chart">
     <PullRequests />
   </div>
@@ -45,10 +22,26 @@
     <a href="specs">View specs</a>
   </div>
 </div>
+<h2>Metrics</h2>
+<div class="row">
+  {#if metrics}
+    <div class="metric-chart">
+      <DeployedChanges data={metrics.changes} />
+    </div>
+    <div class="metric-chart">
+      <LeadTime data={metrics.leadTime} />
+    </div>
+    <div class="metric-chart">
+      <FailureRate data={metrics} />
+    </div>
+  {:else}
+    Loading metrics...
+  {/if}
+</div>
 
 <style>
   :global(body) {
-    margin: 0;
+    margin: 10px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
       'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
       'Helvetica Neue', sans-serif;
@@ -64,5 +57,8 @@
     flex: 1;
     min-width: 30%;
     margin: 5px;
+  }
+  h2 {
+    margin-top: 0;
   }
 </style>
